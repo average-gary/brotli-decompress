b265ab91e85e1bb0fb067e69c77963aedadead16
Gary Krause <gary.krause@mara.com>
2024-04-25 11:22:42 -0400

    feat: decompresses and dumps to file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::{Path, PathBuf};
+use anyhow::{Ok, Result};
+use base64::engine;
+use base64::read::DecoderReader;
 use clap::{arg, command, Parser};
-use log::info;
+use log::{debug, info};
+use std::fs::{self, File};
+use std::io::{self, BufRead, Cursor, Read, Write};
+use std::path::{Path, PathBuf};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -13,12 +15,12 @@ struct Args {
     #[arg(short, long, value_name = "FILE")]
     input_file: PathBuf,
 
-    /// Output location, defaults to input directory
+    /// Output location, defaults to input directory. It always uses the same name as the input
     #[arg(short, long)]
     output: Option<PathBuf>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     pretty_env_logger::init();
     let args = Args::parse();
     let input = args.input_file;
@@ -27,25 +29,64 @@ fn main() {
         None => {
             let mut out = input.clone();
             out.pop();
-            out
-        },
+            let new_name = format!("{}-out", input.file_name().unwrap().to_string_lossy());
+            debug!("{}", new_name);
+            out.join(Path::new(&new_name))
+        }
     };
-    
+
     info!("reading from {}", input.to_string_lossy());
+    info!("writing to {}", output_path.to_string_lossy());
     // File hosts.txt must exist in the current path
-    if let Ok(lines) = read_lines(input) {
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines.flatten() {
-            println!("-------------");
-            println!("{}", line);
+    // let mut result = Vec::new();
+    let output_writer = File::create_new(&output_path);
+    let mut output_writer = match output_writer {
+        std::result::Result::Ok(x) => x,
+        Err(_) => {
+            fs::remove_file(&output_path).unwrap();
+            File::create_new(output_path.clone()).expect("why did we fail after deleting?")
+        }
+    };
+    let lines = read_lines(input)?;
+    // Consumes the iterator, returns an (Optional) String
+    for line in lines.flatten() {
+        let mut wrapper_reader = Cursor::new(line);
+        let base64_reader =
+            DecoderReader::new(&mut wrapper_reader, &engine::general_purpose::STANDARD);
+        let mut reader = brotli::Decompressor::new(
+            base64_reader,
+            4096, // buffer size
+        );
+        let mut buf = [0u8; 4096];
+        loop {
+            match reader.read(&mut buf[..]) {
+                Err(e) => {
+                    if let io::ErrorKind::Interrupted = e.kind() {
+                        continue;
+                    }
+                    panic!("{}", e);
+                }
+                std::result::Result::Ok(size) => {
+                    if size == 0 {
+                        break;
+                    }
+                    output_writer.write_all(&buf[..size])?;
+                    let text =
+                        String::from_utf8(buf[..size].to_vec()).expect("no string from utf8");
+                    debug!("decompressed to: {}", text);
+                }
+            }
         }
     }
+    Ok(())
 }
 
 // The output is wrapped in a Result to allow matching on errors.
 // Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+fn read_lines<P>(filename: P) -> Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
